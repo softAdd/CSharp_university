a7a5d1ef5f7d55c6f1b991e9d5d09258fa08dbe0
softAdd <48087689+softAdd@users.noreply.github.com>
2019-03-09 23:06:00 +0500

    Add files via upload

---
+++ WF2/WF2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WF2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(352, 356);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(221, 395);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(143, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Добавить студента";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WF2/WF2/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Student[] students = new Student[20];
+            students[0] = new Student("Евсей", "ОЭ-102", DateTime.Parse("1999.1.6"), 5, 5, 5);
+            students[1] = new Student("Парфен", "ОЭ-203", DateTime.Parse("1998.12.7"), 3, 3, 3);
+            students[2] = new Student("Назар", "ИТ-201", DateTime.Parse("1997.1.1"), 3, 4, 5);
+            students[3] = new Student("Артемий", "ИП-132", DateTime.Parse("1999.3.22"), 4, 4, 4);
+            students[4] = new Student("Эрнст", "ИТ-201", DateTime.Parse("1995.1.21"), 5, 4, 5);
+            students[5] = new Student("Леондий", "ИТм-205", DateTime.Parse("1999.3.5"), 5, 4, 5);
+            students[6] = new Student("Агап", "ИТм-205", DateTime.Parse("2000.9.9"), 5, 4, 4);
+            students[7] = new Student("Макар", "РЭ-102", DateTime.Parse("1999.2.2"), 5, 4, 5);
+            students[8] = new Student("Афанасий", "РЭ-201", DateTime.Parse("1997.2.3"), 4, 5, 5);
+            students[9] = new Student("Викентий", "ИТм-204", DateTime.Parse("1999.6.8"), 5, 5, 5);
+            students[10] = new Student("Злата", "ИТ-203", DateTime.Parse("2000.11.23"), 5, 5, 5);
+            students[11] = new Student("Антонина", "ИТ-201", DateTime.Parse("1999.3.23"), 3, 4, 5);
+            students[12] = new Student("Майя", "ПТ-202", DateTime.Parse("1999.2.2"), 5, 4, 3);
+            students[13] = new Student("Валентина", "ОЭ-203", DateTime.Parse("1999.11.28"), 5, 5, 5);
+            students[14] = new Student("Ульяна", "ОЭ-203", DateTime.Parse("1999.6.23"), 4, 4, 5);
+            students[15] = new Student("Лидия", "ИТм-205", DateTime.Parse("1999.11.23"), 5, 4, 5);
+            students[16] = new Student("Ника", "РЭ-102", DateTime.Parse("2000.7.22"), 5, 4, 3);
+            students[17] = new Student("Ариадна", "ИТ-201", DateTime.Parse("1999.11.17"), 3, 4, 3);
+            students[18] = new Student("Юлия", "РЭ-201", DateTime.Parse("1998.9.16"), 3, 3, 3);
+            students[19] = new Student("Анисья", "ОЭ-203", DateTime.Parse("1999.11.11"), 4, 4, 5);
+
+            richTextBox1.Text += string.Format("{0, -20}\t{1, -20}\t {2, -15}\t{3, -40}", "Имя", "Группа", "Возраст", "Стипендия") + "\n\n";
+            for (int i = 0; i < students.Length; i++)
+            {
+                var name = students[i].name;
+                var group = students[i].group;
+                var age = students[i].getAge(students[i].dateBirthDay);
+                var scholar = students[i].getScholar(students[i].physMark, students[i].mathMark, students[i].infoMark);
+                var text = string.Format("{0, -20}\t{1, -20}\t {2, -15}\t{3, -40}", name, group, age, scholar);
+                richTextBox1.Text += text + "\n";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        struct Student
+        {
+            public string name;
+            public string group;
+            public DateTime dateBirthDay;
+            public int physMark;
+            public int mathMark;
+            public int infoMark;
+
+            public Student(string name, string group, DateTime dateBirthDay, int physMark, int mathMark, int infoMark)
+            {
+                this.name = name;
+                this.group = group;
+                this.dateBirthDay = dateBirthDay;
+                this.physMark = physMark;
+                this.mathMark = mathMark;
+                this.infoMark = infoMark;
+            }
+
+            public int getAge(DateTime dateBirthDay)
+            {
+                DateTime dateNow = DateTime.Now;
+                int year = dateNow.Year - dateBirthDay.Year;
+                if (dateNow.Month < dateBirthDay.Month ||
+                (dateNow.Month == dateBirthDay.Month && dateNow.Day < dateBirthDay.Day)) year--;
+                return year;
+            }
+            public double getScholar(int physMark, int mathMark, int infoMark)
+            {
+                if (physMark <= 3 || mathMark <= 3 || infoMark <= 3)
+                {
+                    return 0;
+                }
+                double currentScholar = 1300;
+                double coefficient = (double)(physMark + mathMark + infoMark / 3) / 10 + 1;
+                currentScholar *= coefficient;
+                return currentScholar;
+            }
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
