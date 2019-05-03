5b1061ac2a8928e0e039773ea85eda973c2efa33
Maksim Konkov <soft.snake867@gmail.com>
2019-05-03 23:49:38 +0500

    sql

---
+++ SQL(6)/lab6/lab6/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(663, 454);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(693, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "> 5000 рублей";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(693, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(256, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Товары Sumsung";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(693, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(256, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Лето 2012";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(693, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(256, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Пылесосы без акции";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(693, 177);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(256, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Товары и стоимость по скидке с учетом 5 %";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(693, 219);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(256, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Сумма подарков к товарам по акции (300р)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(693, 259);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(256, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Количество Vitec";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(693, 298);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(256, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Бытовая техника";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(693, 352);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(690, 336);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Результат запроса:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 487);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SQL(6)/lab6/lab6/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace lab6
+{
+    public partial class Form1 : Form
+    {
+        public static string connectString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\Soft\Downloads\Алгоритмические языки и системы программирования\Лабораторная работа 6\Бытовая техника.mdb;";
+
+        private OleDbConnection myConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            myConnection = new OleDbConnection(connectString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT Продажи.[код чека], Продажи.дата, Продажи.товар, Ассортимент.фирма, Ассортимент.[базовая стоимость], [вид акций].[вид акции] " +
+                "FROM [вид акций] INNER JOIN (([название товара] INNER JOIN Ассортимент ON [название товара].название = Ассортимент.название) INNER JOIN (акции INNER JOIN Продажи ON акции.код = Продажи.[код акции]) ON Ассортимент.код = Продажи.товар) ON [вид акций].код = акции.название_акции " +
+                "ORDER BY [код чека]";
+            setData(query);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Ассортимент WHERE Ассортимент.[базовая стоимость] > 5000";
+            setData(query);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Ассортимент WHERE Ассортимент.[фирма] = 'Sumsung'";
+            setData(query);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Продажи WHERE Продажи.[дата] BETWEEN #6/1/2011# AND #8/31/2017#";
+            setData(query);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // string query = "SELECT * FROM [Продажи] INNER JOIN [Ассортимент] ON [Продажи].[код чека] = [Ассортимент].[код] WHERE ([код акции] = 7 OR [код акции] = 8) AND ([код чека] = 9 OR [код чека] = 43 OR [код чека] = 45 OR [код чека] = 57 OR [код чека] = 58)";
+            // string query = "SELECT Продажи.[код чека], Продажи.дата, Ассортимент.название FROM Продажи INNER JOIN Ассортимент ON Продажи.[товар] = Ассортимент.код";
+            string query = "SELECT Продажи.[код чека], Продажи.дата, Ассортимент.название, Продажи.[код акции] FROM Продажи INNER JOIN Ассортимент ON Продажи.[товар] = Ассортимент.код " +
+                "WHERE ([код акции] = 7 OR [код акции] = 8) AND ([код чека] = 9 OR [код чека] = 43 OR [код чека] = 45 OR [код чека] = 57 OR [код чека] = 58)";
+            // query = "SELECT акции.код, [вид акций].[вид акции] FROM [акции] INNER JOIN [вид акций] ON [акции].[название_акции] = [вид акций].[код]";
+            setData(query);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM Продажи WHERE Продажи.[код акции] > 0 AND Продажи.[код акции] < 7";
+            setData(query);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT Продажи.[код чека], Продажи.дата, Продажи.товар, Ассортимент.фирма, Ассортимент.[базовая стоимость], [вид акций].[вид акции] " +
+                "FROM [вид акций] INNER JOIN (([название товара] INNER JOIN Ассортимент ON [название товара].название = Ассортимент.название) " +
+                "INNER JOIN (акции INNER JOIN Продажи ON акции.код = Продажи.[код акции]) ON Ассортимент.код = Продажи.товар) ON [вид акций].код = акции.название_акции " +
+                "WHERE Продажи.[код акции] = 4 OR Продажи.[код акции] = 1";
+            setData(query);
+            double giftPrice = 300;
+            giftPrice *= getSelectionCount("Покупка+подарок", 5);
+            textBox1.Text = giftPrice.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM [Ассортимент] WHERE [фирма] = 'Vitec'";
+            setData(query);
+            textBox1.Text = getSelectionCount("Vitec", 2).ToString();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT Продажи.[код чека], Продажи.дата, Продажи.товар, Ассортимент.фирма, Ассортимент.[базовая стоимость], [вид акций].[вид акции] " +
+                "FROM [вид акций] INNER JOIN (([название товара] INNER JOIN Ассортимент ON [название товара].название = Ассортимент.название) INNER JOIN (акции INNER JOIN Продажи ON акции.код = Продажи.[код акции]) ON Ассортимент.код = Продажи.товар) ON [вид акций].код = акции.название_акции " +
+                "ORDER BY [код чека]";
+            setData(query);
+        }
+
+        public void setData(string query)
+        {
+            myConnection.Open();
+            DataSet ds = new DataSet();
+            new OleDbDataAdapter(query, myConnection).Fill(ds);
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+            myConnection.Close();
+            textBox1.Text = "";
+        }
+
+        public int getSelectionCount(string parameter, int column)
+        {
+            if (dataGridView1.RowCount == 0)
+            {
+                return 0;
+            }
+            int result = 0;
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                if (dataGridView1[column, i].Value.ToString() == parameter)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+    }
+}
